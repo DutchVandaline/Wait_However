9df46253799651ad74d70286e724aaf2934f8e67
Jason Park <developerha0013@gmail.com>
2024-11-05 20:40:41 +0900

    added screen

--- lib/Screens/AnalysisScreen.dart
+++ lib/Screens/AnalysisScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:waithowever/Services/ai_services.dart';
 
@@ -60,11 +59,12 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
           textAlign: TextAlign.start,
           text: TextSpan(
             style: TextStyle(
-                fontFamily: "Poppins",
-                fontWeight: FontWeight.w600,
-                fontSize: 30.0,
-                color: Theme.of(context).primaryColorLight,
-                height: 1.05),
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w600,
+              fontSize: 30.0,
+              color: Theme.of(context).primaryColorLight,
+              height: 1.05,
+            ),
             children: const <TextSpan>[
               TextSpan(text: 'Wait, '),
               TextSpan(
@@ -84,96 +84,71 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
                   ),
                 )
               : ListView(
-                  children: _buildCategoryWidgets(),
+                  children: [
+                    KeywordWidget(
+                        keywords: _categorizedResults["keyword"] ?? ""),
+                    TendencyWidget(
+                        content: _categorizedResults["tendency"] ?? ""),
+                    FactsWidget(content: _categorizedResults["facts"] ?? ""),
+                    PerspectiveWidget(
+                        content: _categorizedResults["perspective"] ?? ""),
+                  ],
                 ),
         ),
       ),
     );
   }
+}
 
-  List<Widget> _buildCategoryWidgets() {
-    List<Widget> widgets = [];
+class KeywordWidget extends StatelessWidget {
+  final String keywords;
 
-    _categorizedResults.forEach((key, value) {
-      if (key == "keyword") {
-        List<String> keywords = value.split('/').map((e) => e.trim()).toList();
-        widgets.add(
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Keywords",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      color: Theme.of(context).primaryColorLight,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  SizedBox(
-                    height: 40.0,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: keywords
-                          .map((keyword) => Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 4.0),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: Theme.of(context).canvasColor,
-                                      borderRadius:
-                                          BorderRadius.circular(20.0)),
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10.0, vertical: 5.0),
-                                    child: Center(
-                                      child: Text(
-                                        "# $keyword",
-                                        style: const TextStyle(
-                                            color: Colors.white, fontSize: 17.0),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ))
-                          .toList(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      } else {
-        widgets.add(_buildCard(key.capitalize(), value));
-      }
-    });
-    return widgets;
-  }
+  const KeywordWidget({Key? key, required this.keywords}) : super(key: key);
 
-  Widget _buildCard(String title, String content) {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> keywordList =
+        keywords.split('/').map((e) => e.trim()).toList();
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            title,
-            style: const TextStyle(
+            "Keywords",
+            style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 25,
-              color: Colors.black,
+              color: Theme.of(context).primaryColorLight,
             ),
           ),
           const SizedBox(height: 8),
-          MarkdownBody(
-            data: content,
-            styleSheet: MarkdownStyleSheet(
-              p: const TextStyle(fontSize: 17), // 본문 폰트 크기를 16으로 설정
+          SizedBox(
+            height: 40.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: keywordList
+                  .map((keyword) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).canvasColor,
+                              borderRadius: BorderRadius.circular(20.0)),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 5.0),
+                            child: Center(
+                              child: Text(
+                                "# $keyword",
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 17.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
             ),
           ),
         ],
@@ -182,8 +157,137 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
   }
 }
 
-extension StringExtension on String {
-  String capitalize() {
-    return "${this[0].toUpperCase()}${substring(1)}";
+class TendencyWidget extends StatelessWidget {
+  final String content;
+
+  const TendencyWidget({Key? key, required this.content}) : super(key: key);
+
+  Map<String, int> _extractValues(String text) {
+    final regExp = RegExp(r'(\w+):\s*(-?\d+)');
+    Map<String, int> values = {};
+
+    for (var match in regExp.allMatches(text)) {
+      final tag = match.group(1)!;
+      final value = int.parse(match.group(2)!);
+      values[tag] = value;
+    }
+
+    return values;
   }
+
+  List _getPolicyIcon(int value) {
+    if (value == -3) return [Icons.shield, "보수적"];
+    if (value == -2) return [Icons.balance, "중도"];
+    if (value == -1) return [Icons.attach_money, "진보적"];
+    return [Icons.warning_amber, "문제가 발생했습니다."];
+  }
+
+  List _getAgitationIcon(int value) {
+    if (value == 1) return [Icons.sentiment_satisfied_alt, "낮은 선동성"];
+    if (value == 2) return [Icons.sentiment_neutral, "약한 선동성"];
+    if (value == 3) return [Icons.sentiment_dissatisfied, "보통 선동성"];
+    if (value == 4) return [Icons.sentiment_very_dissatisfied, "다분히 선동적"];
+    if (value == 5) return [Icons.warning, "심히 선동적"];
+    return [Icons.help_outline, "문제가 발생했습니다."];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final values = _extractValues(content);
+    final policyValue = values['policy'] ?? 0;
+    final agitationValue = values['agitation'] ?? 0;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Tendency",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Icon(_getPolicyIcon(policyValue)[0], size: 30),
+                  SizedBox(height: 10.0,),
+                  Text(
+                    _getPolicyIcon(policyValue)[1],
+                    style: const TextStyle(fontFamily: "Poppins", fontSize: 20.0),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 16),
+              Column(
+                children: [
+                  Icon(_getAgitationIcon(agitationValue)[0], size: 30),
+                  SizedBox(height: 10.0,),
+                  Text(
+                    _getAgitationIcon(agitationValue)[1],
+                    style: const TextStyle(fontFamily: "Poppins", fontSize: 20.0),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+}
+
+class FactsWidget extends StatelessWidget {
+  final String content;
+
+  const FactsWidget({Key? key, required this.content}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildCard("Facts", content);
+  }
+}
+
+class PerspectiveWidget extends StatelessWidget {
+  final String content;
+
+  const PerspectiveWidget({Key? key, required this.content}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildCard("Perspective", content);
+  }
+}
+
+Widget _buildCard(String title, String content) {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            color: Colors.black,
+          ),
+        ),
+        const SizedBox(height: 8),
+        MarkdownBody(
+          data: content,
+          styleSheet: MarkdownStyleSheet(
+            p: const TextStyle(fontSize: 17),
+          ),
+        ),
+      ],
+    ),
+  );
 }
